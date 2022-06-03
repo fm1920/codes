7057f37fcc806049dc14e3c62e43dfb9a9ce837e
fm1920 <106371800+fm1920@users.noreply.github.com>
2022-06-03 21:17:44 +0600

    Create average.c

---
+++ average.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    float avg;
+    printf("Enter the values=");
+    scanf("%f%f%f",&a,&b,&c);
+    avg=(float)(a+b+c)/3;
+    printf("the avg is=%f",avg);
+}
